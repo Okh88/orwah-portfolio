7b7397861cb1de5f6c0c830ceec8266c4206da04
Okh88 <orwah.khadrow@gmail.com>
2025-10-30 02:11:01 +0100

    Publish site to /docs with correct base

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
+  base: '/orwah-portfolio/', // must match repo name exactly
 })
